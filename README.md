6a2a706b22a301881a66ab3863ed9957cdc16a66
hgiudatto <hgiudatto@prismamp.com>
2021-12-10 08:24:39 -0300

    Se origina el versionado

--- README.md
+++ README.md
@@ -68,3 +68,6 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+<!-- https://youtu.be/lSMBhUEVK8k?list=PLHVhTB0g5W5FyD7nfStkPjP80ahCuqJp4&t=1687 42:42 -->
+
---
+++ src/context/User/UserReducer.js
@@ -0,0 +1,23 @@
+/* eslint-disable default-case */
+/* eslint-disable import/no-anonymous-default-export */
+import { GET_USERS, GET_PROFILE } from "../types";
+
+export default (state, action) => {
+    const { payload, type } = action;
+
+    switch (type) {
+        case GET_USERS:
+            return {
+                ...state,
+        users: payload,
+      };
+        case GET_PROFILE:
+            return {
+                ...state,
+        selectedUser: payload,
+      };
+        default:
+            return state;
+    }
+
+};
